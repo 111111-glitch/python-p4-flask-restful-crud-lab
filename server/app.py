9d270336deec4826be3ca66e33cbf1643fa664c7
111111-glitch <ngigimelissa@gmail.com>
2024-04-12 13:26:10 +0300

    complete

--- server/app.py
+++ server/app.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python3
-
 from flask import Flask, jsonify, request, make_response
 from flask_migrate import Migrate
 from flask_restful import Api, Resource
@@ -9,7 +7,7 @@ from models import db, Plant
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///plants.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.json.compact = False
+app.json.compact = True
 
 migrate = Migrate(app, db)
 db.init_app(app)
@@ -18,23 +16,19 @@ api = Api(app)
 
 
 class Plants(Resource):
-
     def get(self):
         plants = [plant.to_dict() for plant in Plant.query.all()]
         return make_response(jsonify(plants), 200)
 
     def post(self):
         data = request.get_json()
-
         new_plant = Plant(
             name=data['name'],
             image=data['image'],
-            price=data['price'],
+            price=data['price']
         )
-
         db.session.add(new_plant)
         db.session.commit()
-
         return make_response(new_plant.to_dict(), 201)
 
 
@@ -42,14 +36,35 @@ api.add_resource(Plants, '/plants')
 
 
 class PlantByID(Resource):
-
     def get(self, id):
-        plant = Plant.query.filter_by(id=id).first().to_dict()
-        return make_response(jsonify(plant), 200)
+        plant = Plant.query.get(id)
+        if plant:
+            return make_response(plant.to_dict(), 200)
+        else:
+            return make_response({'message': 'Plant not found'}, 404)
+
+    def patch(self, id):
+        plant = Plant.query.get(id)
+        if not plant:
+            return make_response({'message': 'Plant not found'}, 404)
+
+        data = request.get_json()
+        for key, value in data.items():
+            setattr(plant, key, value)
+        db.session.commit()
+        return make_response(plant.to_dict(), 200)
+
+    def delete(self, id):
+        plant = Plant.query.get(id)
+        if not plant:
+            return make_response({'message': 'Plant not found'}, 404)
+
+        db.session.delete(plant)
+        db.session.commit()
+        return '', 204
 
 
 api.add_resource(PlantByID, '/plants/<int:id>')
-
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
